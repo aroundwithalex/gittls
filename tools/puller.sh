d2766390c6ca7ebdb1e34a011a1700f17687d670
Fred Flintstone <67066474+aroundwithalex@users.noreply.github.com>
2025-02-21 23:53:07 +0000

    Refactor puller command

--- tools/puller.sh
+++ tools/puller.sh
@@ -27,22 +27,43 @@ for repo in $TARGET_DIR/*/; do
 
     cd $repo
 
+    printf "$(tput setaf 2)\nPulling $repo\n"
+
     if [ ! -d .git ]; then
         printf "$(tput setaf 1)\n$repo is not a git repository\n"
         printf "$(tput setaf 1)\nContinuing\n"
         continue
     fi
 
-    if [ $(git branch --show-current) != $TARGET_BRANCH ]; then
+    HAS_CHANGES=$(git status --porcelain)
+    PREV_BRANCH=$(git branch --show-current)
 
-        if [ -z $(git status --porcelain) ]; then
-            git checkout $TARGET_BRANCH
-        elif ! git -C $repo stash; then
-            printf "$(tput setaf 1)\nUnable to stash changes in $repo\n"
+    if [ -n "$HAS_CHANGES" ] && ! git -C $repo stash; then
+        printf "$(tput setaf 1)\nUnable to stash changes in $repo\n"
+        printf "$(tput setaf 1)\nContinuing\n"
+        continue
+    fi
+
+    if ! git -C $repo checkout $TARGET_BRANCH; then
+        printf "$(tput setaf 1)\nUnable to checkout $TARGET_BRANCH for $repo\n"
+        printf "$(tput setaf 1)\nContinuing\n"
+        continue
+    fi
+
+    if ! git -C $repo pull; then
+        printf "$(tput setaf 1)\nUnable to pull $TARGET_BRANCH for $repo\n"
+        printf "$(tput setaf 1)\nContinuing\n"
+        continue
+    fi
+
+    if [ -n "$HAS_CHANGES" && "$PREV_BRANCH" != "$TARGET_BRANCH" ]; then
+        git checkout -q $PREV_BRANCH
+
+        if ! git -C $repo stash pop; then
+            printf "$(tput setaf 1)\nUnable to unstash changes\n"
             printf "$(tput setaf 1)\nContinuing\n"
             continue
         fi
-
-    git pull
+    fi
 
 done
