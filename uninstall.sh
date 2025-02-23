133e51650ce0b90108cf946f635107710cc719d1
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-23 15:49:38 +0000

    Fix bug in uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -36,7 +36,7 @@ read -p "Are you sure you want to uninstall gittls? [y/N] " ACTION
 
 if [[ "$ACTION" =~ ^(y|Y)$ ]]; then
     sudo rm -rf ~/.local/share/gittls
-    sed "/alias gittls='~/.local/share/gittls/run.sh'/d" $(determine_shell)
+    sed '/alias gittls=/d' $(determine_shell)
 else
     printf "$(tput setaf 2)\nPlease update for the latest changes.\n"
     exit 0
